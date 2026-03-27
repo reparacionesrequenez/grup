6cc332bb80a480a3ce823ce8f40841c49ae6a826
reparacionesrequenez <martinreq@gmail.com>
2026-03-27 13:58:23 -0600

    Add files via upload

---
+++ ap.js
@@ -0,0 +1,83 @@
+// CONFIGURACIÓN FIREBASE
+const firebaseConfig = {
+ apiKey: "AIzaSyCLUbc6f6HmkAa3XEHfYeiS2g9HtVmirpU",
+  authDomain: "crud-online-ca595.firebaseapp.com",
+  projectId: "crud-online-ca595"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+// VARIABLE GLOBAL
+let idSeleccionado = null;
+
+// CREATE (Crear)
+function crearUsuario() {
+  const nombre = document.getElementById("nombre").value;
+  const correo = document.getElementById("correo").value;
+
+  db.collection("usuarios").add({
+    nombre: nombre,
+    correo: correo
+  }).then(() => {
+    limpiar();
+    leerUsuarios();
+  });
+}
+
+// READ (Leer)
+function leerUsuarios() {
+  const lista = document.getElementById("lista");
+  lista.innerHTML = "";
+
+  db.collection("usuarios").get().then(snapshot => {
+    snapshot.forEach(doc => {
+      const data = doc.data();
+
+      lista.innerHTML += `
+        <li>
+          ${data.nombre} - ${data.correo}
+          <button onclick="editarUsuario('${doc.id}', '${data.nombre}', '${data.correo}')">Editar</button>
+          <button onclick="eliminarUsuario('${doc.id}')">Eliminar</button>
+        </li>
+      `;
+    });
+  });
+}
+
+// UPDATE (Actualizar)
+function editarUsuario(id, nombre, correo) {
+  document.getElementById("nombre").value = nombre;
+  document.getElementById("correo").value = correo;
+  idSeleccionado = id;
+}
+
+function actualizarUsuario() {
+  const nombre = document.getElementById("nombre").value;
+  const correo = document.getElementById("correo").value;
+
+  db.collection("usuarios").doc(idSeleccionado).update({
+    nombre: nombre,
+    correo: correo
+  }).then(() => {
+    limpiar();
+    leerUsuarios();
+  });
+}
+
+// DELETE (Eliminar)
+function eliminarUsuario(id) {
+  db.collection("usuarios").doc(id).delete().then(() => {
+    leerUsuarios();
+  });
+}
+
+// LIMPIAR FORMULARIO
+function limpiar() {
+  document.getElementById("nombre").value = "";
+  document.getElementById("correo").value = "";
+  idSeleccionado = null;
+}
+
+// INICIALIZAR
+leerUsuarios();
